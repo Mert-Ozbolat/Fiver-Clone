750df73c18999b420c0c8be42ad425e0fe12efe8
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-03 15:21:58 +0300

    update

--- api/src/controllers/gig.controller.ts
+++ api/src/controllers/gig.controller.ts
@@ -1,5 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import c from "../utils/catchAsync.ts";
 
-export const getAllGigs = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
+
+export const getAllGigs = c(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     res.status(200).json({ message: 'İşlem başarili' })
-}
+})
+
+export const getGig = c(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    res.status(200).json({ message: 'İşlem başarili' })
+})
+
+export const createGig = c(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    res.status(200).json({ message: 'İşlem başarili' })
+})
+
+export const deleteGig = c(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    res.status(200).json({ message: 'İşlem başarili' })
+})
--- api/src/routes/gig.routes.ts
+++ api/src/routes/gig.routes.ts
@@ -1,10 +1,10 @@
 import express, { Router } from 'express'
+import { createGig, deleteGig, getAllGigs, getGig } from '../controllers/gig.controller.ts'
 
 const router: Router = express.Router()
 
-// router.route('/').get()
-// router.route('/:id').get()
-
+router.route('/').get(getAllGigs).post(createGig)
+router.route('/:id').get(getGig).delete(deleteGig)
 
 
 export default router
