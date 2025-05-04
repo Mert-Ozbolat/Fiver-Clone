620a0e857b62f1a81bda283147da1ef821852947
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-04 15:34:08 +0300

    update

--- client/src/pages/create/index.tsx
+++ client/src/pages/create/index.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import { inputs } from '../../utils/constants'
+import React, { FormEvent } from 'react'
+import { categories, inputs } from '../../utils/constants'
 import Input from '../../components/input'
+import Select from '../../components/input/select'
 
 const Create = () => {
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+        e.preventDefault()
+
+        const data = new FormData(e.currentTarget)
+    }
+
+
     return (
         <div>
             <h1 className='font-bold text-3xl mb-5'>Yeni Hizmet Oluştur</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-x-10'>
                     {
                         inputs.map((props, key) => (
                             <Input key={key} {...props} />
                         ))
                     }
+                    <Select label='Kategori' options={categories} name='category' />
                 </div>
 
-                <div>
+                <div className='flex md:justify-center my-5'>
                     <button className='bg-green-500 px-6 py-2 rounded-md text-white hover:bg-green-600 max-md:w-full'>Oluştur</button>
                 </div>
 
