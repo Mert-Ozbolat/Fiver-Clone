c6b00dcfad41b2d1ab6aa07ba9ee50911a3f935c
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-04 16:41:40 +0300

    update

--- client/src/pages/search/index.tsx
+++ client/src/pages/search/index.tsx
@@ -1,8 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import api from '../../api'
+import Title from './title'
 
 const Search = () => {
+
+    const [searchParams] = useSearchParams()
+    const query = searchParams.get('query')
+    const category = searchParams.get('category')
+
+    const params = {
+        category,
+        search: query
+    }
+
+    const { isLoading, error, data, refetch } = useQuery({
+        queryKey: ['gigs', params],
+        queryFn: () => api.get('/gigs', { params }).then((res) => res.data.gigs)
+    })
+
+
     return (
-        <div>Search</div>
+        <div>
+            <Title query={query} category={category} />
+        </div>
     )
 }
 
