e14b0c9204829f48d3a9345edb672430214a9de6
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-04 11:03:04 +0300

    update

--- client/src/pages/create/index.tsx
+++ client/src/pages/create/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { inputs } from '../../utils/constants'
+import Input from '../../components/input'
 
 const Create = () => {
     return (
-        <div>Create</div>
+        <div>
+            <h1 className='font-bold text-3xl mb-5'>Yeni Hizmet Oluştur</h1>
+
+            <form>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-x-10'>
+                    {
+                        inputs.map((props, key) => (
+                            <Input key={key} {...props} />
+                        ))
+                    }
+                </div>
+
+                <div>
+                    <button className='bg-green-500 px-6 py-2 rounded-md text-white hover:bg-green-600 max-md:w-full'>Oluştur</button>
+                </div>
+
+            </form>
+        </div>
     )
 }
 
