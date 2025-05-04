91bb0a08fa2609096f8e98ba9113b56e1373c5fd
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-04 18:13:30 +0300

    update

--- client/src/pages/detail/index.tsx
+++ client/src/pages/detail/index.tsx
@@ -1,8 +1,44 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import api from '../../api'
+import Loader from '../../components/loader'
+import Error from '../../components/error'
+import GigInfo from './gig-info'
+import BreadCrumb from './bread-crumb'
+import UserInfo from './user-info'
+import PackageInfo from './package-info'
+import { categories } from './../../utils/constants';
 
 const Detail = () => {
+
+    const { id } = useParams()
+
+    const { isLoading, error, data, refetch } = useQuery({
+        queryKey: ['gig'],
+        queryFn: () => api.get(`/gigs/${id}`).then((res) => res.data.gig)
+    })
+
+
     return (
-        <div>Detail</div>
+        <div>
+            {
+                isLoading ? (
+                    <Loader designs='my-20 size-8' />
+                ) : error ? (
+                    <Error info={error} refetch={refetch} />
+                ) : (
+                    <div>
+                        <div>
+                            <BreadCrumb category={data.category} />
+                            <GigInfo gig={data} />
+                            <UserInfo />
+                        </div>
+                        <PackageInfo gig={data} />
+                    </div>
+                )
+            }
+        </div>
     )
 }
 
