ecb03b6c997c462ef33e6d948153cea7713769a1
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-06 20:11:31 +0300

    update

--- client/src/components/card/buttons.tsx
+++ client/src/components/card/buttons.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { IGig } from "../../types";
+import api from "../../api";
 
-const Buttons = () => {
+type Props = {
+    item: IGig;
+};
+
+const Buttons = ({ item }: Props) => {
+    const client = useQueryClient();
+
+    const { mutate, isPending } = useMutation({
+        mutationFn: () => api.delete(`/gigs/${item._id}`),
+
+        onSuccess: () => {
+            client.invalidateQueries({ queryKey: ["my-gigs"] });
+        },
+    });
+
     return (
-        <div>Buttons</div>
-    )
-}
+        <div className="flex justify-end px-2">
+            <button
+                className="button bg-red-400"
+                disabled={isPending}
+                onClick={() => {
+                    if (confirm("Silmeyi onaylıyor musunuz?")) {
+                        mutate();
+                    }
+                }}
+            >
+                Sil
+            </button>
+        </div>
+    );
+};
 
-export default Buttons
+export default Buttons;
--- client/src/pages/my-gigs/index.tsx
+++ client/src/pages/my-gigs/index.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useAuth } from "../../context/authContext"
+import { useAuth } from "../../context/authContext";
 import api from "../../api";
 import Loader from "../../components/loader";
 import Error from "../../components/error";
@@ -7,8 +7,10 @@ import Card from "../../components/card";
 import { IGig } from "../../types";
 
 const MyGigs = () => {
+    // kullanıcı bilgilerini context'den al
     const { user } = useAuth();
 
+    // kullanıcya ait olan hizmetleri al
     const { isLoading, error, data, refetch } = useQuery<IGig[]>({
         queryKey: ["my-gigs", user],
         queryFn: () =>
@@ -22,17 +24,22 @@ const MyGigs = () => {
             <h1 className="font-bold text-3xl mb-5 text-gray-600">Hizmetlerim</h1>
 
             <div>
-                {
-                    isLoading ? (
-                        <Loader />
-                    ) : error ? (
-                        <Error />
-                    ) : (
-                        data && data.map((gig) => <Card item={gig} expand={true} />)
+                {isLoading ? (
+                    <Loader />
+                ) : error ? (
+                    <Error info={error} refetch={refetch} />
+                ) : (
+                    data && (
+                        <div className="layout">
+                            {data.map((item) => (
+                                <Card key={item._id} item={item} expand />
+                            ))}
+                        </div>
                     )
-                }
+                )}
             </div>
         </div>
-    )
+    );
 };
+
 export default MyGigs;
