26a2dc3e3084649ace22448a6d66a7cf1a31a7af
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-06 19:59:32 +0300

    update

--- client/src/pages/my-gigs/index.tsx
+++ client/src/pages/my-gigs/index.tsx
@@ -1,17 +1,38 @@
 import { useQuery } from "@tanstack/react-query";
 import { useAuth } from "../../context/authContext"
 import api from "../../api";
+import Loader from "../../components/loader";
+import Error from "../../components/error";
+import Card from "../../components/card";
+import { IGig } from "../../types";
 
 const MyGigs = () => {
     const { user } = useAuth();
 
-    const { data } = useQuery({
-        queryKey: ["gigs"],
+    const { isLoading, error, data, refetch } = useQuery<IGig[]>({
+        queryKey: ["my-gigs", user],
         queryFn: () =>
             api
                 .get("/gigs", { params: { userID: user?._id } })
                 .then((res) => res.data.gigs),
     });
-    return <div>MyGigs</div>;
+
+    return (
+        <div>
+            <h1 className="font-bold text-3xl mb-5 text-gray-600">Hizmetlerim</h1>
+
+            <div>
+                {
+                    isLoading ? (
+                        <Loader />
+                    ) : error ? (
+                        <Error />
+                    ) : (
+                        data && data.map((gig) => <Card item={gig} expand={true} />)
+                    )
+                }
+            </div>
+        </div>
+    )
 };
 export default MyGigs;
