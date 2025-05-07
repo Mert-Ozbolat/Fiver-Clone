9ef3dd974aaffcbb9cedea78ce353f54646fa1e6
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-05-07 13:39:41 +0300

    update

--- client/src/components/protected/index.tsx
+++ client/src/components/protected/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useAuth } from '../../context/authContext'
+import Loader from '../loader'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const Protected = () => {
+
+    const { user, isLoading } = useAuth()
+
+    if (isLoading) return <Loader />
+
+    if (!user?.isSeller) return <Navigate to='/' replace />
+
+
     return (
-        <div>Protected</div>
+        <Outlet />
     )
 }
 
